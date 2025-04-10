93fc82ce2f2a8b631f208947a0832dbdf0f65567
Bajder <brooke.jackson@papamoacollege.school.nz>
2025-04-10 12:07:46 +1200

    W.I.P Instructions

--- C_02_instructions.py
+++ C_02_instructions.py
@@ -7,31 +7,49 @@
 # * * * * * * * * * * * * * * * * * * *
 # Ask if they want the instructions
 
-# Check whether they said yes or no, complete functions
-def yes_no(question):
-    """ Checks user response the a question is yes / no (y/n), returns 'yes' or 'no' """
+# Check that users have entered a valid option based on a list
+
+def string_checker(question, valid_ans=("yes", "no")):
+    error = f"Please enter a valid option from the following list: {valid_ans}"
+
     while True:
-        response = input(question).lower()
+        user_response = input(question).lower()
+        for item in valid_ans:
+            # check if the user response is a word in the list
+            if item == user_response:
+                return item
+            # check if the user response is the same as the first letter of an item in the list
+            elif user_response == item[0]:
+                return item
 
-        if response == "yes" or response == "y":
-            return "yes"
-        elif response == "no" or response == "n":
-            return "no"
-        else:
-            print("Please enter a valid input")
-
+        # print error if user does not enter something that is valid
+        print(error)
+        print()
 
 def instructions():
     """ Prints the instructions for the user """
     print("""
 *** Instructions ****
-Roll the dice and try to win!  
+
+To begin, choose the number of rounds (or press enter for infinite mode).
+
+The play against the computer. You need to choose on of the following: R (rock), P (paper), or S (scissors).
+
+The rules are as follows:
+o  Paper beats rock
+o  Rock beats scissors
+o  Scissors beats paper
+
+Good Luck!
     """)
 
 
 # MAIN ROUTINE
-
-want_instructions = yes_no("Do you want to see the instructions? ").lower()
+print()
+print("🪨📃✂️ Rock / Paper / Scissors Game ✂️📃🪨")
+print()
+# Ask the user if they would like to see the instructions
+want_instructions = string_checker("Do you want to read the instructions? ")
 print(f"you chose {want_instructions}")
 
 # Display the instructions if the user wants to see them...
