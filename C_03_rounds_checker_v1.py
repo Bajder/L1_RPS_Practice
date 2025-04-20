7713da6815ca31cfdbe75779a89df7aaf257fb38
Bajder <brooke.jackson@papamoacollege.school.nz>
2025-04-20 15:26:53 +1200

    Create C_03_rounds_checker_v1.py

---
+++ C_03_rounds_checker_v1.py
@@ -0,0 +1,28 @@
+# Check that users have entered a valid option based on a list
+
+
+
+# Automated testing is below in the form (test_case, expected_value)
+to_test = [
+    ("yes", "yes"),
+    ("Y", "yes"),
+    ("No", "no"),
+    ("N", "no"),
+    ("YeS", "yes"),
+    ("Maybe", "invalid")
+]
+
+# run tests!
+for item in to_test:
+    # retrieve test case and expected value
+    case = item[0]
+    expected = item[1]
+
+    # get actual value (ie: test ticket function)
+    actual = string_checker(case, valid_ans=["yes", "no"])
+
+    # compare actual and expected and output pass / fail
+    if actual == expected:
+        print(f" ✅✅✅ Passed! Case: {case}, expected: {expected}, received: {actual} ✅✅✅")
+    else:
+        print(f" ❌❌❌ Failed! Case: {case}, expected: {expected}, received: {actual} ❌❌❌")
